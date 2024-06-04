ec9c96de679393b8bfe4d3c8f8053bd61da36bb4
rjcoder86 <rohitdjadhav789@gmail.com>
2024-06-05 00:58:43 +0530

    logout call modification

--- users/views.py
+++ users/views.py
@@ -11,6 +11,7 @@ User = get_user_model()
 LOGIN_TIME_IN_DAYS = 14
 LOGIN_TIME = LOGIN_TIME_IN_DAYS * 24 * 3600
 
+
 class RegisterUserView(APIView):
     permission_classes = [AllowAny]
 
@@ -31,22 +32,33 @@ class CustomLoginView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        response=Response()
+        response = Response()
         serializer = CustomTokenObtainPairSerializer(data=request.data)
         if serializer.is_valid():
-            response.set_cookie('refresh_token', serializer.validated_data.get('refresh', None), httponly=True,secure=True, max_age=LOGIN_TIME,samesite='None')
-            response.set_cookie('token', serializer.validated_data.get('refresh', None), httponly=True,domain="new.keywordio.com",secure=True, max_age=LOGIN_TIME,samesite=None)
-            response.data=serializer.validated_data
+            response.set_cookie('refresh_token', serializer.validated_data.get(
+                'refresh', None), httponly=True, secure=True, max_age=LOGIN_TIME, samesite='None')
+            response.set_cookie('token', serializer.validated_data.get(
+                'refresh', None), httponly=True, domain="new.keywordio.com", secure=True, max_age=LOGIN_TIME, samesite=None)
+            response.data = serializer.validated_data
             response.status_code = status.HTTP_200_OK
             return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LogoutView(APIView):
+
     def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token', None)
+        response = Response()
         try:
-            refresh_token = request.data["refresh"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
+            if refresh_token is None:
+                raise ValueError("Refresh token not found.")
+
+            response.set_cookie('refresh_token', httponly=True, max_age=1)
+            response.status_code = status.HTTP_200_OK
+            response.data = {"message": "User Logged out successfully"}
+
         except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            response.data = {'error': str(e)}
+            response.status_code = status.HTTP_400_BAD_REQUEST
+        return response
